72ae5b3b9803170315ecef3dd36e04b2059a4bde
1Miguel <jmjugsimar@gmail.com>
2025-02-05 22:59:23 +0800

    checkpoint update

--- src/cstruct/__init__.py
+++ src/cstruct/__init__.py
@@ -8,11 +8,16 @@ from enum import IntEnum
 from typing import Tuple, Dict, Any, Literal, TypeVar, TypedDict
 from typing_extensions import Self
 
+__all__ = (
+    "Cstruct",
+    "u8",
+)
 
-_FIELDS = "__cstruct_fields__"
+_CSTRUCT_FIELDS = "__cstruct_fields__"
+_CSTRUCT_DESC = "__cstruct_desc__"
 
 
-u8 = TypeVar("u8")
+u8 = TypeVar("u8", int, float)
 i8 = TypeVar("i8")
 u16 = TypeVar("u16")
 i16 = TypeVar("i16")
@@ -23,7 +28,8 @@ i64 = TypeVar("i64")
 
 
 _TYPE_MAP = {
-    u8: "b"
+    u8: "b",
+    i8: "q",
 }
 
 
@@ -31,11 +37,7 @@ def is_cstruct(obj_or_type) -> bool:
     """
     __doc__
     """
-    return hasattr(obj_or_type, _FIELDS)
-
-
-class _Missing:
-    """Missing type."""
+    return hasattr(obj_or_type, _CSTRUCT_FIELDS)
 
 
 class _Field(TypedDict):
@@ -45,6 +47,15 @@ class _Field(TypedDict):
     enum: IntEnum
     default: Any
     len: int
+    is_array: bool
+    struct: str
+
+
+class Array:
+    """"""
+
+    def __init__(self, length: int = 0) -> None:
+        self.length = length
 
 
 class _CstructMeta(type):
@@ -54,34 +65,54 @@ class _CstructMeta(type):
         name: str,
         bases: Tuple[type, ...],
         namespace: Dict[str, Any],
-        endianness: Literal["little", "big"],
+        endianness: Literal["little", "big"] = "little",
     ) -> "_CstructMeta":
         """"""
-        annon = mcs.__annotations__
         inst = type.__new__(mcs, name, bases, namespace)
+        annon = inst.__annotations__
         fields = {}
+        desc = ""
 
         if annon:
-            for i in annon:
-                pass
+            for i, (n, t) in enumerate(annon.items()):
+                if n in fields:
+                    raise ValueError(f"field '{n}' already exists")
+
+                val = getattr(inst, n, None)
+                fields = {"name": n, "type": t, "default": val}
+                meta = getattr(t, "__metadata__", None)
+
+                if meta and isinstance(meta[0], Array):
+                    fields["length"] = meta[0].length
+                    fields["is_array"] = True
+                    fields["type"] = t.__origin__
+                elif meta is not None:
+                    raise TypeError(f"invalid type metadata '{meta}'")
+
+                if isinstance(fields["type"], IntEnum):
+                    fields["enum"] = fields["type"]
+                    # packed the enum to its appropriate size
+                    
+                # this is used to compile to struct
+                # if the array size if unknown, add a marker
+                # this will be filled up later by the array size
+
+        setattr(inst, _CSTRUCT_FIELDS, fields)
+        setattr(inst, _CSTRUCT_DESC, desc)
 
         return inst
 
+class Cstruct(metaclass=_CstructMeta, endianness="little"):
 
-class _Cstruct(endianness="little", metaclass=_CstructMeta):
-
-    def from_buffer(self) -> Self:
+    @classmethod
+    def from_buffer(cls, data: bytes) -> Self:
         """
         __dest__
         """
-        return _Cstruct()
+        return Cstruct()
 
-    def __bytest__(self) -> bytes:
+    def __bytes__(self) -> bytes:
         """
         __dest__
         """
         pass
-
-
-class Cstruct:
-    pass
--- test/test_packing.py
+++ test/test_packing.py
@@ -1,14 +1,18 @@
 import unittest
 
-import cstruct
+from typing import Annotated
+from cstruct import Cstruct, Array, u8
 
 class TestPacking(unittest.TestCase):
 
-
     def test_pack_unsigned(self) -> None:
-        class TestStruct(cstruct.Cstruct):
-            pass
+        class TestStruct(Cstruct):
+            my_u8: Annotated[u8, Array(4)]
+        print(TestStruct.__annotations__)
 
+    def test_pack_signed(self) -> None:
+        class TestStruct(Cstruct):
+            my_i8: u8
 
 if __name__ == "__main__":
     unittest.main()
